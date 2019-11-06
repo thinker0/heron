cf2806ceb5814b182e19786bfcf5c989d3383d14
thinker0 <thinker0@gmail.com>
2019-11-06 17:49:39 +0900

    wip

--- scripts/get_all_heron_paths.sh
+++ scripts/get_all_heron_paths.sh
@@ -31,6 +31,7 @@ bazel build \
   --incompatible_new_actions_api=false \
   --incompatible_depset_is_not_iterable=false \
   --incompatible_no_support_tools_in_action_inputs=false \
+  --incompatible_disallow_unverified_http_downloads=false \
   --config=`platform` {heron,integration_test,tools/java,examples,heronpy,storm-compatibility,storm-compatibility-examples,eco,eco-storm-examples,eco-heron-examples}/...
 result=$?
 if [ "${result}" -eq "0" ] ; then
